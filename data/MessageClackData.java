41c08f9b30d71f6c2b88dbabaaad7d5f4b071ccc
StephenMiner222 <70287119+StephenMiner222@users.noreply.github.com>
2020-09-10 10:33:20 -0400

    Update MessageClackData.java

--- data/MessageClackData.java
+++ data/MessageClackData.java
@@ -1,20 +1,69 @@
 package data;
 
+/**
+ * Subclass of ClackData for when a user sends a message
+ * 
+ * @author Stephen Miner
+ *
+ */
+
 public class MessageClackData extends ClackData {
 	
 	private String message;
 	
+	/**
+	 * constructor to set up the username, mesage, and type. Calls a constructor from it's superclass ClackData
+	 * @param userName
+	 * @param message
+	 * @param type
+	 */
 	MessageClackData(String userName, String message, int type) {
 		super(userName, type);
 		this.message = message;
 	}
 	
+	/**
+	 * default constructor, calls the constructor above which relies on the superclass
+	 */
 	MessageClackData(){
 		this("Anon", "", 0);
 	}
 	
+	/**
+	 * return the instant message
+	 */
 	public String getData() {
 		return this.message;
 	}
 	
+	
+	/**
+	 * override of equals, compares the userName, type, and message
+	 * @param otherMessage
+	 * @return
+	 */
+	public boolean equals(MessageClackData otherMessage) {
+		return (this.getUserName() == otherMessage.getUserName()
+				&& this.getType() == otherMessage.getType()
+				&& this.message == otherMessage.message);
+	}
+	
+	/**
+	 * override of hashCode, uses the userName, type, and message
+	 */
+	public int hashCode() {
+		int hash = 17;
+		hash = 37 * hash + getUserName().hashCode();
+		hash = 37 * hash + getType();
+		hash = 37 * hash + message.hashCode();
+		return hash;
+	}
+	
+	/**
+	 * returns a full description of the object including the super class instance variables
+	 */
+	public String toString() {
+		return("UserName: " + getUserName() + "\n" + "" );
+	}
+
 }
