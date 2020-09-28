6fa6f1f4b714ce8613374e3af7381324f5acfc8a
JonathanNordby <70287268+JonathanNordby@users.noreply.github.com>
2020-09-28 13:28:05 -0400

    Add files via upload

--- ClackClient.java
+++ ClackClient.java
@@ -1,6 +1,11 @@
 package main;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 import data.ClackData;
+import data.FileClackData;
+import data.MessageClackData;
 
 /**
  * The Client version of the Clack program
@@ -15,6 +20,9 @@ public class ClackClient {
 	private boolean closeConnection;
 	private ClackData dataToSendToServer, dataToReceiveFromServer;
 	private final static int DEFAULT_PORT = 7000;
+	private Scanner inFromStd;
+	private final String KEY = "Armavirumquecano";
+	
 	
 	public ClackClient(String userName, String hostName) {
 		this(userName, hostName, DEFAULT_PORT);
@@ -28,19 +36,57 @@ public class ClackClient {
 		this("anonymous", "localhost", DEFAULT_PORT);
 	}
 	
-	public ClackClient(String userName, String hostName, int port) {
-		
+	public ClackClient(String userName, String hostName, int port) {	
+		if (port < 1024) {
+			throw new IllegalArgumentException("Invalid port number!");
+		}
 		this.userName = userName;
 		this.hostName = hostName;
 		this.port = port;
 	}
 	
+	/**
+	 * Initializes a Client
+	 */
 	public void start() {
 		//TODO Implement Later
+		inFromStd = new Scanner(System.in);
+		closeConnection = false;
+		do {
+			readClientData();
+			printData();
+		} while(!closeConnection);
+		inFromStd.close();
 	}
 	
+	/**
+	 * Reads in data provided in standard input
+	 */
 	public void readClientData() {
 		//TODO Implement Later
+		String input;
+		if (inFromStd.hasNext()) {
+			input = inFromStd.nextLine();
+			if (input.startsWith("DONE")) {
+				closeConnection = true;
+				dataToSendToServer = new MessageClackData(userName, "DONE", KEY, ClackData.CONSTANT_SENDMESSAGE);
+			} else if (input.startsWith("SENDFILE")) {
+				dataToSendToServer = new FileClackData(userName, input.substring("SENDFILE".length() + 1), ClackData.CONSTANT_SENDFILE);
+				if (dataToSendToServer instanceof FileClackData) {
+						try {
+							((FileClackData) dataToSendToServer).readFileContents(KEY);
+						} catch (IOException e) {
+							System.err.println("Error: unable to read file.DON");
+						}
+						((FileClackData) dataToSendToServer).writeFileContents(KEY);
+				}
+			} else if (input == "LISTUSERS") {
+				System.out.println("Not Yet Supported");
+				//NOTHING YET
+			} else {
+				dataToSendToServer = new MessageClackData(userName, input, KEY, ClackData.CONSTANT_SENDMESSAGE);
+			}
+		}
 	}
 	
 	public void sendData() {
@@ -50,9 +96,11 @@ public class ClackClient {
 	public void receiveData() {
 		//TODO Implement Later
 	}
-	
+	/**
+	 * Prints out the data that is to be sent to the server
+	 */
 	public void printData() {
-		//TODO Implement Later
+		System.out.println(dataToSendToServer.getData(KEY));
 	}
 	
 	public String getUserName() {
@@ -86,12 +134,40 @@ public class ClackClient {
 		}
 		ClackClient otherClient = (ClackClient) other;
 		
-		return userName == otherClient.userName &&
-			   hostName == otherClient.hostName &&
-			   port == otherClient.port &&
-			   closeConnection == otherClient.closeConnection &&
-			   dataToSendToServer.equals(otherClient.dataToSendToServer) && 
-			   dataToReceiveFromServer.equals(otherClient.dataToReceiveFromServer);
+		if (dataToSendToServer == null && otherClient.dataToSendToServer == null) {
+			if (dataToReceiveFromServer == null && otherClient.dataToReceiveFromServer == null) {
+				return userName == otherClient.userName &&
+					   hostName == otherClient.hostName &&
+					   port == otherClient.port &&
+					   closeConnection == otherClient.closeConnection;
+			} else {
+				return userName == otherClient.userName &&
+					   hostName == otherClient.hostName &&
+					   port == otherClient.port &&
+					   closeConnection == otherClient.closeConnection &&
+					   dataToReceiveFromServer.equals(otherClient.dataToReceiveFromServer);				
+			}
+		} else if (dataToSendToServer != null && otherClient.dataToSendToServer != null) {
+			if (dataToReceiveFromServer != null && otherClient.dataToReceiveFromServer != null) {
+				return userName == otherClient.userName &&
+					   hostName == otherClient.hostName &&
+					   port == otherClient.port &&
+					   closeConnection == otherClient.closeConnection &&
+					   dataToSendToServer.equals(otherClient.dataToSendToServer) && 
+					   dataToReceiveFromServer.equals(otherClient.dataToReceiveFromServer);
+			} else {
+				if (dataToReceiveFromServer == null && otherClient.dataToReceiveFromServer == null) {
+					return userName == otherClient.userName &&
+							   hostName == otherClient.hostName &&
+							   port == otherClient.port &&
+							   closeConnection == otherClient.closeConnection &&
+							   dataToSendToServer.equals(otherClient.dataToSendToServer);
+				}
+				return false;
+			}
+		} else {
+			return false;
+		}	
 	}
 	
 	@Override
